bdf118e56649df56696e7cd2cb7eed551fdb24ee
EaseTheWorld <easetheworld@gmail.com>
2012-09-01 12:11:32 +0900

    change the url in description.

--- src/dev/easetheworld/animator/AnimatorPlayer.java
+++ src/dev/easetheworld/animator/AnimatorPlayer.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  * 
- * https://github.com/EaseTheWorld/PaintAnimatorTest
+ * https://github.com/EaseTheWorld/PaintAnimator
  */
 
 package dev.easetheworld.animator;
